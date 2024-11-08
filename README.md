f6f677251ffaf69f6e7d6c9be53b504b9ab56851
monoxideboi <38110094+monoxideboi@users.noreply.github.com.>
2024-11-07 22:21:28 -0600

    init

---
+++ README.md
@@ -0,0 +1,98 @@
+# Lastfm rest api cloudflare worker
+
+This is a cloudflare worker that will create a restapi that will return your most recently/currently playing song! This lets you keep your api key yours and hidden while caching requests!
+
+If you do not have a cloudflare account go get it!
+
+If you do not want to use the [cli](#CLI) do it with the [webapp](#webapp)
+
+## CLI
+
+Deploy
+```
+npm create cloudflare@latest -- --template https://github.com/monoxideboi/steam-cf-worker.git
+```
+name it steamcfworker, or whatever you want
+```
+cd steamcfworker
+```
+
+The eddit the wrangler.toml file, replace the ID with your steam ID and the games array with a list of game IDs that you want to add
+
+```
+npx wrangler deploy
+```
+You will need to get an [api key from steam](https://steamcommunity.com/dev/apikey). Once you do, do
+`npx wrangler secret put KEY`
+and put in your key in the input afterwards.
+
+By default the cache time is 5 minutes, feel free to increase this to whatever you want in [the index.js](./src/index.js)!
+
+## Webapp
+
+Go to [the cloudflare workers page](https://workers.cloudflare.com/), click log in and go to the dashboard.
+
+Click Create Worker, and name it whatever you want. Create it.
+
+Then click edit code, and copy paste [the index.js](./src/index.js), replacing the original code. Ignore the error messages.
+
+Then go back with the arrow in the top left (and save), and click on settings. Click the `Variables and Secrets` tab. Click the + Add button to add a variable.
+
+First make the ID variable, name it ID and add your steam ID. You can find it from your steam profile url, or if you have a custom url go to your profile -> edit profile -> custom url and delete the custom name right below to see the ID. It should be only numbers.
+
+Next make the GAMES variable, name it GAMES make the value like this (with brackets): [<gameid>,<gameid>,<gameid>]. You can find a steam game's id from the url like this
+```
+https://store.steampowered.com/app/440/Team_Fortress_2/ -> 440
+```
+For example your games field should be like: `[632360,960090,1743850,504230]`
+
+Then make the KEY variable, make the value your API key. Feel free to encrypt this one.
+
+You're done! If you want a custom domain keep reading!
+
+## Custom Domain
+
+If you really want a custom domain, go to the worker dashboard and go to your worker's settings, there will be a tab called `Domains & Routes` that you can set a custom domain for.
+
+## Custom Routes
+If you want to make your api link like this: `example.com/steam`, you can go to the worker routes and route the worker to whatever you want!
+
+## Usage
+Just use fetch to get the contents of your REST api. The json object return has three objects, games, currentgame, and recentgames.
+
+### games
+Returns an object for each of the IDs you put in. The key is whatever game ID it refers to. It returns
+```
+steamID: idk tbh
+ganeName: name of game
+achievements: achievments of the game (named weird, if achieved is 1 that means its complete)
+stats: stats the game has chosen to give
+playtime_forever: playtime in minutes
+rtime_last_played: uts of time last played
+playtime_2weeks: playtime over 2 weeks in minutes
+```
+
+### currentgames
+Returns an object with your most recently played game.
+```
+gamename: game name
+gameid: game's steam id
+lastplayed: uts of time last played
+isplaying: if game is currently being played
+```
+
+### recentgames
+Returns an object with your recent games and the number of them.
+```
+total_count: number of games in array
+games: array of games
+    appid: steam id
+    name: name of game
+    playtime_2weeks: playtime over 2 weeks in minutes
+    playtime_forever: playtime in minutes
+    img_icon_url: the hash of the image, combine it with the steam id to get the image like so: http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{hash}.jpg
+    playtime_(windows/mac/linux/deck)_forever: playtime according to individual systems (ngl not that important ig)
+```
+
+## Help
+My discord is [monoxideboi](https://discord.com/users/375379813403328523), feel free to DM or ping me if you need help.
---
+++ src/index.js
@@ -0,0 +1,141 @@
+export default {
+	async fetch(request, env, ctx) {
+		console.log('setting kv');
+		await env.RECENTGAME.put('key', 'text');
+		console.log(await env.RECENTGAME.get('key'));
+		const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${env.KEY}&steamid=${env.ID}&format=json`;
+		const cacheUrl = new URL(url);
+
+		// Construct the cache key from the cache URL
+		const cacheKey = new Request(cacheUrl.toString());
+		const cache = caches.default;
+
+		// Check whether the value is already available in the cache
+		// if not, you will need to fetch it from origin, and store it in the cache
+		let response = await cache.match(cacheKey);
+
+		if (!false) {
+			console.log(`Response for request url: ${url} not present in cache. Fetching and caching request.`);
+			// If not in cache, get it from origin
+
+			let finaljson = {};
+
+			let add = {};
+			for (let i = 0; i < env.GAMES.length; i++) {
+				let json = await fetch(
+					`http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=${env.GAMES[i]}&key=${env.KEY}&steamid=${env.ID}`
+				)
+					.then((data) => {
+						return data.json();
+					})
+					.then((json) => {
+						// console.log(json);
+						return json;
+					});
+				add[env.GAMES[i]] = json.playerstats;
+			}
+
+			await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${env.KEY}&steamid=${env.ID}&format=json`)
+				.then((data) => {
+					return data.json();
+				})
+				.then((json) => {
+					// console.log(json.response);
+					let games = json.response.games;
+					// console.log(res);
+					console.log(games.length);
+					for (let i = 0; i < games.length; i++) {
+						// console.log(games[i].appid);
+						// console.log(add[games[i].appid]);
+						if (add[games[i].appid]) {
+							console.log(`game ${games[i].appid} found`);
+							add[games[i].appid].platime_forever = games[i].playtime_forever;
+							add[games[i].appid].rtime_last_played = games[i].rtime_last_played;
+							if (games[i].playtime_2weeks) {	
+								add[games[i].appid].playtime_2weeks = games[i].playtime_2weeks;
+							} else {
+								add[games[i].appid].playtime_2weeks = 0;
+							}
+						}
+					}
+				});
+
+			// console.log(add)
+			finaljson.games = add;
+
+			let userjson = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${env.KEY}&steamids=${env.ID}`)
+				.then((data) => {
+					return data.json();
+				})
+				.then((json) => {
+					// console.log(json);
+					return json.response.players[0];
+				});
+
+			if (userjson.gameextrainfo) {
+				let store = {
+					gamename: userjson.gameextrainfo,
+					gameid: userjson.gameid,
+					lastplayed: Date.now(),
+				};
+				await env.RECENTGAME.put('recentgame', JSON.stringify(store));
+				store.isplaying = true;
+				finaljson.currentgame = store;
+			} else {
+				let recentgame = JSON.parse(await env.RECENTGAME.get('recentgame'));
+
+				finaljson.currentgame = recentgame;
+				finaljson.currentgame.isplaying = false;
+			}
+
+			let recentjson = await fetch(url)
+				.then((data) => {
+					return data.json();
+				})
+				.then((json) => {
+					// console.log(json);
+					return json.response;
+				});
+
+
+			finaljson.recentgames = recentjson;
+
+			// let recentjson = await fetch(url).then(data => {
+			// 	return data.json();
+			// }).then(json => {
+			// 	console.log(json);
+			// 	return json.response;
+			// });
+
+			// finaljson = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${env.KEY}&steamid=${env.ID}&include_played_free_games=1&format=json`)
+			// .then((data) => {
+			// 	return data.json();
+			// })
+			// .then((json) => {
+			// 	// console.log(json);
+			// 	// for (let i = 0; i < json.length; i++) {
+			// 	// 	if (.)
+			// 	// }
+			// 	return json.response;
+			// });
+
+			// console.log(finaljson);
+
+			// Must use Response constructor
+			response = Response.json(finaljson);
+
+			// Cache API respects Cache-Control headers. Setting s-max-age to 300
+			// will limit the response to be in cache for 5 minutes max
+			response.headers.append('Cache-Control', 's-maxage=300');
+
+			response.headers.append('Access-Control-Allow-Origin', '*');
+			response.headers.append('Access-Control-Allow-Headers', '*');
+
+			// Any changes made to the response here will be reflected in the cached value
+			ctx.waitUntil(cache.put(cacheKey, response.clone()));
+		} else {
+			console.log(`Cache hit for: ${url}.`);
+		}
+		return response;
+	},
+};
